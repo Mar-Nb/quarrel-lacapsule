0b8d164f92a2b70485b7b817106984e8224de33e
Martin Niombela <martin-stephen.niombela@iroko.eu>
2023-11-23 15:21:51 +0100

    Setup Firebase 2

---
+++ services/firebase-skeleton.ts
@@ -0,0 +1,7 @@
+// import { initializeApp } from 'firebase/app';
+
+// const firebaseConfig = {
+// TODO: Add your Firebase configuration here
+// };
+
+// const app = initializeApp(firebaseConfig);
